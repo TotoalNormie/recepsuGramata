69ae04379fe06cf5228f6f2d26c3d3c5c63a8e3a
mr-jap <85937382+mr-jap@users.noreply.github.com>
2023-10-17 09:00:21 +0300

    finished DB interface

--- backend/database_interface.php
+++ backend/database_interface.php
@@ -15,9 +15,17 @@
 			return $this->DB->connect_error;
 		}
 
+		public function CreateRecipe($title, $description, $image_url, $ingredient_json)
+		{
+			$stmt = $this->DB->prepare("INSERT INTO recipes(title, description, image_url, ingredient_json) VALUES(?, ?, ?, ?)");
+			
+			$stmt->bind_param("ssss", $title, $description, $image_url, $ingredient_json);
+			$stmt->execute();
+		}
+
 		public function ListRecipes()
 		{
-			$result = $this->DB->query("SELECT ID, title, image_url FROM recipes");
+			$result = $this->DB->query("SELECT ID, title, image_url, views FROM recipes");
 			if($result->num_rows<=0)
 				return false;
 
@@ -26,30 +34,56 @@
 
 		public function GetRecipeByID($id)
 		{
-
-		}
-
-		public function DeleteRecipeByID($id)
-		{
-			$stmt = $this->DB->prepare("DELETE FROM ingredients WHERE recipe_id=?");
+			$stmt = $this->DB->prepare("SELECT title, description, image_url, ingredient_json FROM recipes WHERE ID=? LIMIT 1");
 			
 			$stmt->bind_param("i", $id);
 			$stmt->execute();
 
+			$result = $stmt->get_result()
+			if($result->num_rows<=0)
+				return false;
 
+			return $result->fetch_array(MYSQLI_ASSOC);
+		}
+
+		public function DeleteRecipeByID($id)
+		{
 			$stmt = $this->DB->prepare("DELETE FROM recipes WHERE ID=?");
 			
 			$stmt->bind_param("i", $id);
 			$stmt->execute();
 		}
 
-		public function UpdateRecipeByID($id, $title, $description, $image_url, $recipe_json)
+		public function UpdateRecipeByID($id, $title, $description, $image_url, $ingredient_json)
 		{
+			$stmt = $this->DB->prepare("UPDATE recipes SET title=?, description=?, image_url=?, ingredient_json=? WHERE ID=? LIMIT 1");
 			
+			$stmt->bind_param("ssssi", $title, $description, $image_url, $ingredient_json, $id);
+			$stmt->execute();
 		}
+
+		public function IncrementViewsByID($id)
+		{
+			$stmt = $this->DB->prepare("UPDATE recipes SET views = views + 1 WHERE ID=? LIMIT 1");
+			
+			$stmt->bind_param("i", $id);
+			$stmt->execute();
+		}
+
 		public function IsRecipeOwner($recipe_id, $user_identifier)
 		{
+			$stmt = $this->DB->prepare("SELECT users.identifier FROM recipes WHERE recipes.ID=?
+			                            INNER JOIN users ON recipes.owner = users.ID
+			                            LIMIT 1");
+			
+			$stmt->bind_param("i", $recipe_id);
+			$stmt->execute();
 
+			$result = $stmt->get_result();
+			if($result->num_rows<=0)
+				return false;
+
+			return $result->fetch_array(MYSQLI_NUM)[0] == $user_identifier;
 		}
 
 		public function RegisterUser($username, $password)
@@ -57,8 +91,8 @@
 			$stmt = $this->DB->prepare("INSERT INTO users(identifier, username, passhash) VALUES(?, ?, ?)");
 			
 			// these have to be references
-			$identifier = GenerateUserID($username, $password); 
 			$passhash = password_hash($password,  PASSWORD_BCRYPT);
+			$identifier = GenerateUserID($username, $passhash); 
 
 			$stmt->bind_param("sss", $identifier, $username, $passhash);
 			$stmt->execute();
@@ -66,7 +100,31 @@
 
 		public function UserExists($username)
 		{
+			$stmt = $this->DB->prepare("SELECT ID FROM users WHERE username=?");
 			
+			$stmt->bind_param("s", $username);
+			$stmt->execute();
+
+			$result = $stmt->get_result()
+			if($result->num_rows<=0)
+				return false;
+			else
+				return true;
+		}
+
+		public function GetUserIdentifier($username, $password)
+		{
+			$stmt = $this->DB->prepare("SELECT identifier FROM users WHERE username=? AND passhash=?");
+			
+			$passhash = password_hash($password,  PASSWORD_BCRYPT);
+			$stmt->bind_param("ss", $username, $passhash);
+			$stmt->execute();
+
+			$result = $stmt->get_result()
+			if($result->num_rows<=0)
+				return false;
+
+			return $result->fetch_array(MYSQLI_NUM)[0];
 		}
 	}
 ?>
