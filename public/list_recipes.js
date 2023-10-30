6e4e59e186860c7907242f1d3d1f56fa27be3632
mr-jap <85937382+mr-jap@users.noreply.github.com>
2023-10-30 11:02:42 +0200

    recipe list

--- public/list_recipes.js
+++ public/list_recipes.js
@@ -3,6 +3,8 @@ const recipeTemplate = document.getElementById('recepieTemplate');
 
 let SearchBar = document.querySelector("#searchBar > input[type=\"text\"]");
 
+let ViewSorting = {value: "DESC"}; // value: "DESC" | value: "none"
+
 let RecipeList = [];
 
 function DisplayRecipes()
@@ -15,7 +17,6 @@ function DisplayRecipes()
 		if(recipe.hidden)
 			continue;
 
-		console.log(recipe);
 		const cr = recipeTemplate.content.cloneNode(true); // cloned recipe, shortened for conviniance
 
 		cr.querySelector('a').href = 'recipe.html?id=' + recipe.ID;
@@ -25,8 +26,6 @@ function DisplayRecipes()
 		cr.querySelector('span').textContent = recipe.views;
 
 		RecipeContainer.appendChild(cr);
-		
-		console.log(cr);
 
 	}
 	// for(let i = 0; i < RecipeList.length; ++i)
@@ -43,9 +42,41 @@ function DisplayRecipes()
 	// }
 }
 
+function SortRecipes()
+{
+	if(ViewSorting.value !== "none")
+	{
+		if(ViewSorting.value === "ASC")
+		{
+			RecipeList.sort(function(a, b)
+			{			
+				if(a.views > b.views)
+					return 1;
+				else if(a.views < b.views)
+					return -1;
+				else
+					return 0;
+			});
+		}
+		else
+		{
+			RecipeList.sort(function(a, b)
+			{			
+				if(a.views < b.views)
+					return 1;
+				else if(a.views > b.views)
+					return -1;
+				else
+					return 0;
+			});
+		}
+	}
+}
+
 function FilterAndLoad()
 {
-	let searchValue = SearchBar.value;
+	let searchValue = SearchBar.value;	
+
 	for(let i = 0; i<RecipeList.length; ++i)
 	{
 		let thisRecipe = RecipeList[i]; // indexed array lookups are expensive
@@ -64,6 +95,7 @@ function FilterAndLoad()
 }
 
 SearchBar.addEventListener("input", FilterAndLoad);
+//ViewSorting.addEventListener("change", SortRecipes)
 
 function UpdateRecipes()
 {
@@ -77,8 +109,9 @@ function UpdateRecipes()
 				if(response.status == "Success")
 				{
 					RecipeList = response.data;
-
-					DisplayRecipes();
+					
+					SortRecipes();
+					FilterAndLoad();
 				}
 			}else {
 				console.error(this.responseText);
