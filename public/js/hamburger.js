13aeaaaca6a08991a9cb1c13c47722807a0dfef1
Vitaly Juno <vvjuhno@gmail.com>
2023-11-06 09:10:19 +0200

    aeef

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -1,14 +1,11 @@
 const menu_btn = document.querySelector('.hamburber');
 const mobile_menu = document.querySelector('header');
 
-let changusCount = 0;
-
 console.log(menu_btn);
 function hamburberButton() {
 	menu_btn.classList.toggle('is-active');
 	mobile_menu.classList.toggle('is-active');
-	changusCount++;
-	console.log('are the times changus has been found');
+	console.log(' - chungus counter');
 }
 
 setTimeout(() => {
